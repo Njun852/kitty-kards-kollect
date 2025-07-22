c9c958742f337169c3ed9a62cb884ad52d6742b1
Njun852 <njunjuntillaz@gmail.com>
2025-07-22 21:27:27 +0800

    worked on routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,11 @@
 import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router';
+import { useLoaderData } from 'react-router';
 
 function App() {
-  let navigate = useNavigate();
-  const isLoggedIn = false;
-  useEffect(()=>{
-    if(!isLoggedIn) {
-      navigate("/login")
-    }
-  }, [])
+  const {isLoggedIn} = useLoaderData()
   return (
     <>
-      <h1>Dashboard</h1>     
+      <h1>{isLoggedIn ? "Welcome" : "Please login"}</h1>     
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,41 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import 'font-awesome/css/font-awesome.min.css';
-import { BrowserRouter, Routes, Route } from "react-router";
+import "font-awesome/css/font-awesome.min.css";
+import { RouterProvider, createBrowserRouter, redirect } from "react-router";
 import App from "./App.jsx";
 import Login from "./pages/login/Login.jsx";
 
+async function loginLoader({ params }) {}
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: App,
+        loader: ({ params }) => {
+            const isLoggedIn = true;
+            if (!isLoggedIn) {
+                return redirect("/login");
+            }
+            return { isLoggedIn };
+        },
+        action: async ({request})=>{
+            let formData = await request.formData();
+            let username = formData.get("username")
+            let password = formData.get("password")
+            return {username, password}
+        }
+    },
+    {
+        path: "login",
+        Component: Login,
+        loader: ({ params }) => {
+            const isLoggedIn = false;
+            if (isLoggedIn) {
+                return redirect("/");
+            }
+            return { isLoggedIn };
+        },
+    },
+]);
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App/>}/>
-            <Route path="login" element={<Login/>}/>
-        </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
 );
